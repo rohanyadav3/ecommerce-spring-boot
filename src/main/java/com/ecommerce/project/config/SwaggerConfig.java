382c2681dcf20e99541a60f3e203244bcc63f43a
Rohan <ry953128@gmail.com>
2026-09-15 12:45:57 +0530

    Update Swagger project information

--- src/main/java/com/ecommerce/project/config/SwaggerConfig.java
+++ src/main/java/com/ecommerce/project/config/SwaggerConfig.java
@@ -1,11 +1,9 @@
 package com.ecommerce.project.config;
 
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +14,7 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
         SecurityScheme bearerScheme = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
@@ -27,17 +26,11 @@ public class SwaggerConfig {
 
         return new OpenAPI()
                 .info(new Info()
-                        .title("Spring Boot eCommerce API")
+                        .title("E-Commerce API")
                         .version("1.0")
-                        .description("This is a Spring Boot Project for eCommerce")
-                        .license(new License().name("Our License Link").url("http://embarkx.com"))
+                        .description("REST API for an E-Commerce application built using Spring Boot.")
                         .contact(new Contact()
-                                .name("Faisal Memon")
-                                .email("faisal@embarkx.com")
-                                .url("https://github.com/EmbarkxOfficial")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("Project Documentation")
-                        .url("http://embarkx.com"))
+                                .name("Rohan Yadav")))
                 .components(new Components()
                         .addSecuritySchemes("Bearer Authentication", bearerScheme))
                 .addSecurityItem(bearerRequirement);
